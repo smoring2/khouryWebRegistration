80a24a2a784751c7f9f46340d7d3cb5a62bcb4eb
Zhaoyang <zhaoywang99@gmail.com>
2022-11-07 16:58:45 -0800

    Update files in sql folder.
    In this commit, we update expect result pdf file and make some changes to the 5th query in the dbSQL.sql file. Now, SQL 5 would generate all courses the student has completed give the student nuid.

--- sql/dbSQL.sql
+++ sql/dbSQL.sql
@@ -1,4 +1,4 @@
-/* SQL 1: Select the student id and student name with the highest GPA in the given department. */
+-- SQL 1: Select the student id and student name with the highest GPA in the given department.
 select d.department_name, s.nuid, s.name, s.grade as 'dept_highest_gpa'
 from student s
 inner join department d
@@ -7,14 +7,14 @@ where s.department_id = '1'
 order by grade
 limit 1;
 
-/* SQL 2: Given the student's name, get which department the student belongs to. */
+-- SQL 2: Given the student's name, get which department the student belongs to.
 select d.department_name
 from department d
 inner join student s
 on s.department_id = d.department_id
 where s.name = 'Lily';
 
-/* SQL 3: Select max GPA from each department. */
+-- SQL 3: Select max GPA from each department.
 select department_name, grade
 from department d,
 	(select department_id, max(grade) as grade
@@ -23,7 +23,8 @@ from department d,
 ) c
 where d.department_id = c.department_id;
 
-/* SQL 4: Given the selected term, return all courses with the course number, instructor name, campus, meeting time, and course capacity. */
+-- SQL 4: Given the selected term, return all courses with the course number, instructor name, campus, meeting time,
+-- and course capacity.
 select c.course_id, i.name, cam.campus_name, c.meeting_time, c.max_num_of_students as capacity
 from course c
 inner join instructor i
@@ -33,7 +34,10 @@ on cam.campusid = i.campusid
 where c.semester = 2;
 
 
-/* SQL 5: Given the student ID, return all the information of this student. */
-select *
-from student
-where nuid = 1;
+-- SQL 5: Given the student ID, return all courses that the student have completed.
+select s.name, r.course_id as course_completed
+from student s
+inner join registration r
+on s.nuid = r.nuid
+and r.completed = true
+and s.nuid = 1;
