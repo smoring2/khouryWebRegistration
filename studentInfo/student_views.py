4d27d37ef103139657a7676f69dba1681d3b5c6b
Zhaoywang <zhaoywang99@gmail.com>
2022-12-04 14:14:57 -0800

    Update mygrade, course registration page
    In this commit, we:
    
    1. Update mygrade page, show courses in progress.
    2. When register courses, student doesn't need to input advisor id.

--- studentInfo/student_views.py
+++ studentInfo/student_views.py
@@ -5,6 +5,7 @@ from django.db import connection
 from django.http import HttpResponse
 cursor= connection.cursor()
 
+
 def testmysql(request):
     myStudent = Student.objects.all()
     context = {
@@ -17,18 +18,18 @@ class StudentList(ListView):
     template_name= 'student/student_list.html'
     model = Student
 
+
 # Student home page.
 class StudentHome(DetailView):
     template_name = 'student/student_home.html'
     model = Student
 # Student main page after logging in.
+
+
 class StudentDetails(DetailView):
     template_name = 'student/student_profile.html'
     model = Student
 
-class StudentRegistration(DetailView):
-    template_name = 'student/course_registration.html'
-    model = Student
 
 def getStudentNotification(request, student_id):
     res = {
@@ -36,15 +37,8 @@ def getStudentNotification(request, student_id):
         'pending_list': [],
     }
 
-    student_id = {'student_id': int(student_id)}
-    cursor.execute('''SELECT course_id FROM registration WHERE status = 'pending' AND nuid = %(student_id)s''',
-                   student_id)
-    pending_list = cursor.fetchall()
-    if pending_list:
-        for r in pending_list:
-            if r[0] in res['pending_list']:
-                continue
-            res['pending_list'].append(str(r[0]) + "            " + getNameByCourseNum(r[0]))
+    for r in getPendingApprovals(student_id):
+        res['pending_list'].append(str(r) + "            " + getNameByCourseNum(r))
 
     context = {
         "data": res
@@ -52,25 +46,24 @@ def getStudentNotification(request, student_id):
 
     return render(request, 'student/student_notification.html', context)
 
+
 def dropCourse(request, student_id):
     if request.method == 'POST':
         nuid = int(request.POST['s_nuid'])
         course_id = int(request.POST['course_number'])
-        advisor_id = int(request.POST['advisor_id'])
+        advisor_id = getAdvisorByStudentId(student_id)
 
         cursor.execute('''SELECT * from registration''')
         all_registration_info = cursor.fetchall()
-        cursor.execute('''SELECT employee_id from advisor''')
-        advisor_list = cursor.fetchall()
 
         if nuid in all_registration_info[0] and course_id in all_registration_info[1] and advisor_id in all_registration_info[2]:
             return HttpResponse('Error, you cannot submit the same form.')
         elif nuid != student_id:
             return HttpResponse('Please correct your niud.')
-        elif nuid in all_registration_info[0] and course_id not in all_registration_info[1]:
-            return HttpResponse('You cannot drop this course')
         else:
-            Registration.objects.create(nuid = nuid, course_id = course_id, advisor_id = advisor_id, status = 'drop')
+            drop_course = Registration.objects.get(course=course_id, nuid=nuid)
+            drop_course.delete()
+            return HttpResponse('deleted')
 
     res = {
         'student_id': int(student_id),
@@ -82,10 +75,14 @@ def dropCourse(request, student_id):
 
     return render(request, 'student/drop_course.html', context)
 
+
 def getDegreeAudit(request, student_id):
     res = {
         'student_id': int(student_id),
         'completed_list': [],
+        'overall_gpa': [],
+        'cumulative': [],
+        'in_progress': getCourseInProgress(student_id),
     }
 
     cursor.execute('''SELECT * FROM registration WHERE status = 'completed' ''')
@@ -106,13 +103,16 @@ def getDegreeAudit(request, student_id):
                 # Semester hours.
                 sh = cursor.fetchall()[0][0]
                 cum_sh += sh
-                cum_gpa += sh * comp[3]
+                cum_gpa += sh * min(4.0, comp[3])
                 str_builder = "Course: " + str(comp[1]) + "                                       " + grade_map[sh]
                 res['completed_list'].append(str_builder)
 
 
-    res['completed_list'].append("Cumulative: " + str(cum_sh) + "           " + str(cum_gpa))
-    res['completed_list'].append("Overall GPA: " + str(cum_gpa / cum_sh))
+    res['cumulative'].append("Cumulative: " + str(cum_sh) + "           " + str(cum_gpa))
+    if cum_sh == 0:
+        res['overall_gpa'].append("Overall GPA: " + str(0))
+    else:
+        res['overall_gpa'].append("Overall GPA: " + str(cum_gpa / cum_sh))
 
     context = {
         "data": res
@@ -120,20 +120,18 @@ def getDegreeAudit(request, student_id):
 
     return render(request, 'student/degree_audit.html', context)
 
+
 def getRegistrationInfo(request, student_id):
     # Submit course registration form.
     if request.method == 'POST':
         nuid = int(request.POST['s_nuid'])
         course_id = int(request.POST['course_number'])
-        advisor_id = int(request.POST['advisor_id'])
+        advisor_id = getAdvisorByStudentId(student_id)
 
         cursor.execute('''SELECT * from registration''')
         all_registration_info = cursor.fetchall()
         cursor.execute('''SELECT employee_id from advisor''')
         advisor_list = cursor.fetchall()
-        # num_regis = {'course_num': course_id}
-        # cursor.execute('''SELECT registered_num_of_stud FROM course WHERE course_id = %(course_num)s''', num_regis)
-        # num_regis = 10
 
         # Invalid case 1: Student has submitted course registration form or has taken previously.
         for i in range(len(all_registration_info[0])):
@@ -144,8 +142,12 @@ def getRegistrationInfo(request, student_id):
             return HttpResponse('You cannot help others to register courses.')
         elif not isValidAdvisorId(advisor_list, advisor_id):
             return HttpResponse('Please re-enter your advisor id.')
+        elif isCourseFull(course_id):
+            return HttpResponse('The course you registered for has reached the capacity')
         else:
-            Registration.objects.create(nuid = nuid, course_id = course_id, advisor_id = advisor_id, grade = None, status = 'pending')
+            Registration.objects.create(nuid = nuid, course_id = course_id, advisor_id = advisor_id, grade = None,
+                                        status = 'pending')
+            return HttpResponse("Registration succeed, please waiting the advisor for approval")
 
     nuid = int(student_id)
     res = {
@@ -163,23 +165,18 @@ def getRegistrationInfo(request, student_id):
             cursor.execute('''SELECT name FROM instructor WHERE employee_id = %(employee_id)s''', employee_id)
 
             string_builder = "Course Number: " + str(course_id[0]) + "   " + course_id[1] + \
-            "   Meeting time: " + str(course_id[2]) + "   Capacity: " + str(course_id[3]) + \
+            "   Meeting time: " + str(course_id[2]) + "   Capacity: " + str(course_id[4]) + \
                 "   Registered: " + str(course_id[6])
             res['course_list'].append(string_builder)
 
-    student_id = {'student_id': int(student_id)}
-    cursor.execute('''SELECT course_id FROM registration WHERE nuid = %(student_id)s AND grade >= 1.0''',
-                   student_id)
-    results = cursor.fetchall()
-    if results:
-        for r in results:
-            res['complete_courses'].append(r[0])
+    res['complete_courses'] = getCompleteCoursesByNuid(nuid)
 
     context = {
         "data": res
     }
 
     return render(request, 'student/course_registration.html', context)
+
 
 # This method takes two variable, arr stands for a fetched two dimensional array,
 # check if the emplpoyee_id is a valid or not.
@@ -190,7 +187,80 @@ def isValidAdvisorId(arr, employee_id):
 
     return False
 
+
 def getNameByCourseNum(course_id):
     course_id = {'course_id': course_id}
     cursor.execute('''SELECT course_name FROM course WHERE course_id = %(course_id)s''', course_id)
     return cursor.fetchall()[0][0]
+
+
+# Retrieve all information from the course list.
+def getCourseList():
+    res = []
+    cursor.execute('''SELECT * FROM course''')
+    course_list = cursor.fetchall()
+    if course_list:
+        for course_id in course_list:
+            res.append(course_id)
+
+    return res
+
+
+# Input takes student NUID, query the registration table, retrieve all courses id that the student has completed.
+def getCompleteCoursesByNuid(student_id):
+    res = []
+    student_id = {'student_id': student_id}
+    cursor.execute('''SELECT course_id FROM registration WHERE nuid = %(student_id)s AND status = 'completed' ''',
+                   student_id)
+    results = cursor.fetchall()
+
+    if results:
+        for r in results:
+            res.append(r[0])
+
+    return res
+
+
+# Input takes a student NUID, get all course numbers that the student submitted registration form and still pending
+# for advisor approval.
+def getPendingApprovals(student_id):
+    res = []
+    student_id = {'student_id': int(student_id)}
+    cursor.execute('''SELECT course_id FROM registration WHERE status = 'pending' AND nuid = %(student_id)s''',
+                   student_id)
+    pending_list = cursor.fetchall()
+    if pending_list:
+        for r in pending_list:
+            if r[0] in res:
+                continue
+            res.append(r[0])
+
+    return res
+
+
+def getAdvisorByStudentId(student_id):
+    student_id = {'student_id': student_id}
+    cursor.execute('''SELECT advisor FROM student WHERE nuid = %(student_id)s''', student_id)
+    return cursor.fetchall()[0][0]
+
+
+def isCourseFull(course_id):
+    course_id = {'course_id': course_id}
+    cursor.execute('''SELECT max_num_of_students FROM course WHERE course_id = %(course_id)s''', course_id)
+    cap = cursor.fetchall()[0][0]
+    cursor.execute('''SELECT registered_num_of_stud FROM course WHERE course_id = %(course_id)s''', course_id)
+    cur_num = cursor.fetchall()[0][0]
+    return cur_num >= cap
+
+
+def getCourseInProgress(student_id):
+    res = []
+    student_id = {'student_id': student_id}
+    cursor.execute('''SELECT course_id FROM registration WHERE nuid = %(student_id)s AND status = 'approved'
+    ''', student_id)
+    l = cursor.fetchall()
+    for course in l:
+        res.append(course)
+    if len(l) == 0:
+        return []
+    return l[0]
